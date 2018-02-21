4a70b26aa86e020b4c48ed7fdde34deffca75afe
Sidhant Mathur <sidhant185@gmail.com>
2018-02-21 13:04:12 -0600

    Added description, and new examples to list

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 
-let exampleList = ['Shelf','Book','Political Idealogy','Laptop','Smartphone','Printer','Bicycle','Backpack','Boots','Sofa','Movie'];
+let exampleList = ['Shelf','Book','Political Idealogy','Laptop','Smartphone','Printer','Bicycle','Backpack',
+'Boots','Sofa','Movie','TV','Smart Speaker','Headphones','Smart Watch','Watch','Washing Machine','Dryer',
+'Dishwasher','Coffee Table','Desk','Mouse','Dog','Cat','Pet','Coffee Shop','Coffee Beans','Tea Leaves','Tires',
+'Smart Home','Reddit App','Camera','Programming Language','IDE','Text Editor','Industry','Job',];
 
 function newExample() {
     let randomNumber = Math.floor(Math.random() * (exampleList.length));
