fd5031af9ed385880a9a1ea14ca6cc2090ec7b3f
Sidhant Mathur <sidhant185@gmail.com>
2018-02-26 12:55:51 -0600

    Allowed for multiple buttons, changed some flavor content

--- scripts.js
+++ scripts.js
@@ -4,7 +4,17 @@ let exampleList = ['Shelf','Book','Political Idealogy','Laptop','Smartphone','Pr
 'Dishwasher','Coffee Table','Desk','Mouse','Dog','Cat','Pet','Coffee Shop','Coffee Beans','Tea Leaves','Tires',
 'Smart Home','Reddit App','Camera','Programming Language','IDE','Text Editor','Industry','Job',];
 
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+
+btn1.addEventListener("click", newExample);
+btn2.addEventListener("click", newExample);
+
+var spans = Array.prototype.slice.call(document.querySelectorAll('.changeText'));
+
 function newExample() {
     let randomNumber = Math.floor(Math.random() * (exampleList.length));
-    document.getElementById('changeText').innerHTML = exampleList[randomNumber];
+    spans.forEach(function(span){
+        span.textContent = exampleList[randomNumber];
+    });
 }
